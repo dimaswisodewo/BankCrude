47db80e9bd176b596c008bd7c2cd3b2fcc5e6755
dimaswisodewo <mdimaswisodewo26@cjkt.idserve.net>
2026-06-18 14:46:29 +0700

    Initial Commit

---
+++ BankCrude/BankCrudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankCrudeApp.swift
+//  BankCrude
+//
+//  Created by Meynabel Dimas Wisodewo on 18/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BankCrudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
